f682eb698391ce66bca53622529cfc56511fcabc
Ivan Lebedev <40741270+theproudswan@users.noreply.github.com>
2021-03-10 22:36:11 +0300

    Add files via upload

---
+++ yelow-belt/4-week/find_starts_with_char.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(
+    RandomIt range_begin, RandomIt range_end,
+    char prefix){
+    string target1 = {prefix};
+    auto first = lower_bound(range_begin, range_end, target1);
+
+    auto next = static_cast<char>(prefix + 1);
+    string target2 = {next};
+    auto last = upper_bound(range_begin, range_end, target2);
+
+    return make_pair(first, last);
+    }
+
+int main() {
+  const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
+  
+  const auto m_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
+  for (auto it = m_result.first; it != m_result.second; ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+  
+  const auto p_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), 'p');
+  cout << (p_result.first - begin(sorted_strings)) << " " <<
+      (p_result.second - begin(sorted_strings)) << endl;
+  
+  const auto z_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), 'z');
+  cout << (z_result.first - begin(sorted_strings)) << " " <<
+      (z_result.second - begin(sorted_strings)) << endl;
+  
+  return 0;
+}
---
+++ yelow-belt/4-week/find_starts_with_string.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+template <typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(
+    RandomIt range_begin, RandomIt range_end,
+    const string& prefix){
+
+    auto left = lower_bound(range_begin, range_end, prefix);
+    string up_b = prefix;
+
+    ++up_b[up_b.size() - 1];
+
+    auto right = lower_bound(range_begin, range_end, up_b);
+    
+    return {left, right};
+}
+
+int main() {
+  const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
+  
+  const auto mo_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+  for (auto it = mo_result.first; it != mo_result.second; ++it) {
+    cout << *it << " ";
+  }
+  cout << endl;
+  
+  const auto mt_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+  cout << (mt_result.first - begin(sorted_strings)) << " " <<
+      (mt_result.second - begin(sorted_strings)) << endl;
+  
+  const auto na_result =
+      FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+  cout << (na_result.first - begin(sorted_strings)) << " " <<
+      (na_result.second - begin(sorted_strings)) << endl;
+  
+  return 0;
+}
